ab51618033dc2d7f3a5d8669cc1c101f5043ad3e
Artur Barbosa <arturbarbosap@gmail.com>
2026-05-15 21:02:50 -0300

    Program.cs configurado

--- MoneyAPI/Program.cs
+++ MoneyAPI/Program.cs
@@ -1,18 +1,114 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MoneyAPI.Data;
+using Quartz;
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+
+//Dependency Injection Scrutor
+builder.Services.Scan(scan =>
+{
+    scan.FromAssemblyOf<Program>()
+    .AddClasses(c => c.InNamespaces("MoneyAPI.Repositories"))
+        .AsImplementedInterfaces()
+        .WithScopedLifetime()
+    .AddClasses(c => c.InNamespaces("MoneyAPI.Services"))
+        .AsImplementedInterfaces()
+        .WithScopedLifetime();
+});
+
+builder.Services.AddSingleton<Session>();
+
+//BD Entity Framework
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("Default"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("Default")),
+        assembly => assembly.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName));
+});
+
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClients", policy =>
+    {
+        policy
+            .WithOrigins(builder.Configuration
+                .GetSection("AllowedOrigins")
+                .Get<string[]>() ?? [])
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// Quartz
+/*builder.Services.AddQuartz(q =>
+{
+    TODO: Pre_lancamento
+});
+
+builder.Services.AddQuartzHostedService(opt => opt.WaitForJobsToComplete = true);*/
+
+//Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Money API",
+        Version = "v1",
+        Description = "API do Money"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Informe o token no formato: Bearer {token}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "Bearer",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Money API v1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseCors("AllowClients");
 
 app.UseHttpsRedirection();
 
